97d26f8cd2e9ad4d2a1c70d79c9c5452016fe83f
azizsaad740-debug <azizsaad740@gmail.com>
2025-11-19 15:37:49 +0500

    [dyad] Added ThemeProvider wrapper in main.tsx. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import "./globals.css";
 import { SessionContextProvider } from "./integrations/supabase/SessionContextProvider.tsx";
 import SettingsInitializer from "./components/layout/SettingsInitializer.tsx";
 import React from "react";
+import { ThemeProvider } from "./components/layout/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SettingsInitializer>
       <SessionContextProvider>
-        <App />
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
       </SessionContextProvider>
     </SettingsInitializer>
   </React.StrictMode>
