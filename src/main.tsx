b0dca2526e0003e45d2caebf803b1fe904085bdc
azizsaad740-debug <azizsaad740@gmail.com>
2025-11-19 15:35:02 +0500

    [dyad] Updated main.tsx to include SettingsInitializer and SessionContextProvider. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { SessionContextProvider } from "./integrations/supabase/SessionContextProvider.tsx";
+import SettingsInitializer from "./components/layout/SettingsInitializer.tsx";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <React.StrictMode>
+    <SettingsInitializer>
+      <SessionContextProvider>
+        <App />
+      </SessionContextProvider>
+    </SettingsInitializer>
+  </React.StrictMode>
 );
